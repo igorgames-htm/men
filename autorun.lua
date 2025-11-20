490b2df93a7a7455d8822201855f73b1fc2b7fc4
igorgames-htm <sigmasigma14653@gmail.com>
2025-11-20 21:16:14 +0100

    Create autorun.lua

---
+++ autorun.lua
@@ -0,0 +1,10 @@
+local listgame = {
+  [124575823366391] = "obf_oL4Gj34NzB461w2NgY7Jf204o3in2CU521iRVVFLTAWDr23b0yvs1767sALWAyGj.lua",
+  [100854882203609] = "obf_oX2LP2X35OICsLiOaAoTNpJr1s8v5r33yPUW3MLFvkz60VVrOTEM5goti7521Rj9.lua",
+  [140317247681516] = "obf_5x9Uc0Xy7d4TbF93IY81r3ml4PH7xJhwlygQF2uT7uvW6DlKc8veKBA9H1X005Ye.lua",
+  [9285238704] = "obf_QbaXwG7PiOl2PIXF50GYG5YV5mjIZ9jT2R17L9974tXmDi00yV5lQyh489jWfy61.lua",
+}
+
+if listgame[game.PlaceId] then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/igorgames-htm/men/refs/heads/main/"..listgame[game.PlaceId]))()
+end

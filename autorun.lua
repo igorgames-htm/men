1c2dd0a2ce5988a1d32528f7df25343dbadfeab4
igorgames-htm <sigmasigma14653@gmail.com>
2025-11-22 00:24:42 +0100

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -3,6 +3,7 @@ local listgame = {
   [100854882203609] = "obf_oX2LP2X35OICsLiOaAoTNpJr1s8v5r33yPUW3MLFvkz60VVrOTEM5goti7521Rj9.lua",
   [140317247681516] = "obf_5x9Uc0Xy7d4TbF93IY81r3ml4PH7xJhwlygQF2uT7uvW6DlKc8veKBA9H1X005Ye.lua",
   [9285238704] = "obf_QbaXwG7PiOl2PIXF50GYG5YV5mjIZ9jT2R17L9974tXmDi00yV5lQyh489jWfy61.lua",
+  [13772394625] = "obf_t2jFGF297sN3sc2tq5t7v404feR0KpEDKn8cHmkxjq5ig8Pv0wTXHxX2ETo32w4c.lua",
 }
 
 if listgame[game.PlaceId] then
